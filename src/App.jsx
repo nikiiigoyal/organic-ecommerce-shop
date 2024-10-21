490ffef46176dae2b7c449660dba3efde0b60f72
nikiiigoyal <nikiiigoyal@gmail.com>
2024-10-21 16:13:01 +0530

    component of headpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Homepage from "./components/homepage";
 import { SignInForm } from "./components/SignInform";
 import { CreateForm } from "./components/CreateAccount";
+import { ErrorPage } from "./components/ErrorPage";
 
 
 
@@ -15,6 +16,7 @@ return (
   <Homepage />
   <SignInForm />
   <CreateForm />
+  <ErrorPage />
 </div>
 </Router>
 )
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+
+export function ErrorPage () {
+    return (
+        <>
+        <div className="mx-auto font-poppins flex flex-col items-center w-[70%] my-8">
+            <img src="/src/components/images/errorpage.png" alt = "image">
+            </img>
+            <h2 className="font-bold text-4xl text-[#1A1A1A] mt-4 mb-3">Oops! page not found</h2>
+            <p className="text-[18px] text-gray-500 w-[50%] mb-5">Ut consequat ac tortor eu vehicula. Aenean accumsan purus eros. Maecenas sagittis tortor at metus mollis</p>
+            <button className="bg-[#00B207] text-white rounded-full py-3 px-8 mx-auto">Back to Home</button>
+        </div>
+        </>
+    )
+}
