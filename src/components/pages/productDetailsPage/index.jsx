830e377e52a5b1a2eabe84c5e6c7bc28f82c790a
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-08 17:41:28 +0530

    Comment out all the pages of details tab

--- src/components/pages/productDetailsPage/index.jsx
+++ src/components/pages/productDetailsPage/index.jsx
@@ -1,21 +1,21 @@
-/* eslint-disable react/prop-types */
-// import { ProductQuickModal } from './../shopPage/./ProductQuickModal'
-import { useState } from "react"
-import { TabNavigation } from "./TabNavigation"
-import { DescriptionsTab } from "./DescriptionsTab"
-import { AdditionalInfo } from "./AdditionalInfo"
+// /* eslint-disable react/prop-types */
+// // import { ProductQuickModal } from './../shopPage/./ProductQuickModal'
+// import { useState } from "react"
+// import { TabNavigation } from "./TabNavigation"
+// import { DescriptionsTab } from "./DescriptionsTab"
+// import { AdditionalInfo } from "./AdditionalInfo"
 
 
-export const ProductDetails = ({productId = 2}) => {
-    const [activeTab , setActiveTab] = useState('Descriptions')
-    return (
-        <>
-        <div>
-            {/* <ProductQuickModal /> */}
-            <TabNavigation activeTab={activeTab} setActiveTAb={setActiveTab} />
-      {activeTab === 'Descriptions' && <DescriptionsTab productId={productId}/>}
-      {activeTab === 'Additional Information' && <AdditionalInfo productId={productId} />}
-        </div>
-        </>
-    )
-}
+// export const ProductDetails = ({productId = 2}) => {
+//     const [activeTab , setActiveTab] = useState('Descriptions')
+//     return (
+//         <>
+//         <div>
+//             {/* <ProductQuickModal /> */}
+//             <TabNavigation activeTab={activeTab} setActiveTAb={setActiveTab} />
+//       {activeTab === 'Descriptions' && <DescriptionsTab productId={productId}/>}
+//       {activeTab === 'Additional Information' && <AdditionalInfo productId={productId} />}
+//         </div>
+//         </>
+//     )
+// // 
