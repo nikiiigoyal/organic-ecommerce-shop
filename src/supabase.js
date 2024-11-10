b995c7bf53fbc0106dd77eb943d54d72b41b9db8
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-10 23:26:50 +0530

    Close Icon got dispaaeraed so restored it

--- src/supabase.js
+++ src/supabase.js
@@ -5,6 +5,6 @@ const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_PROJECTURL
 
 
 const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANONKEY
-console.log(supabaseUrl,supabaseKey)
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
