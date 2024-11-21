2460ea8ae4ca47379daaa245bc55e3d1741f8de6
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-21 22:42:04 +0530

    Design navigation page

---
+++ src/components/pages/Navigation.jsx
@@ -0,0 +1,34 @@
+const Navigation = () => {
+    return (
+        <>
+        <div className="flex flex-col border border-[#E6E6E6] p-5 m-5">
+        <h1 className="text-[#1A1A1A] text-[20px] mb-2">Navigation</h1>
+        <div className="flex flex-row mx-1 items-center">
+            <img src="images/dashboard.png"></img>
+            <h2 className="text-sm text-[#666666]">Dashboard</h2>
+        </div>
+        <div className="flex flex-row mx-1 items-center">
+            <img src="images/orderHistory.png"></img>
+            <h2 className="text-sm text-[#666666]">Order History</h2>
+        </div>
+        <div className="flex flex-row mx-1 items-center">
+            <img src="images/dashboard.png"></img>
+            <h2 className="text-sm text-[#666666]">Wishlist</h2>
+        </div>
+        <div className="flex flex-row mx-1 items-center">
+            <img src="images/cart.png"></img>
+            <h2 className="text-sm text-[#666666]">Shopping Cart</h2>
+        </div>
+        <div className="flex flex-row mx-1 items-center">
+            <img src="images/settings.png"></img>
+            <h2 className="text-sm text-[#666666]">Settings</h2>
+        </div>
+        <div className="flex flex-row mx-1 items-center">
+            <img src="images/logout.png"></img>
+            <h2 className="text-sm text-[#666666]">Log out</h2>
+        </div>
+        </div>
+        </>
+    )
+}
+export default Navigation;
