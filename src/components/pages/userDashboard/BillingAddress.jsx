39d5d8bafa219c772a002456b0b657b95e90ab2b
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-22 15:07:51 +0530

    Changes in design of Billing address

--- src/components/pages/userDashboard/BillingAddress.jsx
+++ src/components/pages/userDashboard/BillingAddress.jsx
@@ -6,14 +6,14 @@ export const BillingAddress = ({address}) => {
     return(
         <>
         <div className="flex flex-col border border-[#E6E6E6] mx-auto p-3">
-            <h3 className="text-sm text-[#999999]">BILLING ADDRESS</h3>
-            <h2 className="text-[#1A1A1A] font-semibold">{address.fullName}</h2>
-            <p className="text-[#666666] text-sm">{address.streetAddress}</p>
-      <p className="text-[#666666] text-sm">{address.city}, {address.state} {address.zipCode}</p>
+            <h3 className="text-sm text-[#999999] mb-2">BILLING ADDRESS</h3>
+            <h2 className="text-[#1A1A1A] font-semibold mb-1">{address.fullName}</h2>
+            <p className="text-[#666666] text-sm mb-1">{address.streetAddress}</p>
+      <p className="text-[#666666] text-sm mb-1">{address.city}, {address.state} {address.zipCode}</p>
       
-      <p className="text-[#1A1A1A] text-sm">{address.email}</p>
-      <p  className="text-[#1A1A1A] text-sm">{address.phone}</p>
-      <button className="text-[#00B207] text-sm">Edit Address</button>
+      <p className="text-[#1A1A1A] text-sm mb-1">{address.email}</p>
+      <p  className="text-[#1A1A1A] text-sm mb-2">{address.phone}</p>
+      <button className="text-[#00B207] text-sm text-left mb-2">Edit Address</button>
         </div>
         </>
     )
