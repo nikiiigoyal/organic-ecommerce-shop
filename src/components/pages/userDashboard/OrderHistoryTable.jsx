0cfc8ff376427d30a39f7705077048729ee765fd
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-22 07:30:43 +0530

    Changes in design of History table

--- src/components/pages/userDashboard/OrderHistoryTable.jsx
+++ src/components/pages/userDashboard/OrderHistoryTable.jsx
@@ -2,29 +2,29 @@
 const OrderHistoryTable = ({ orders }) => {
     return (
         <>
-        <div className="flex flex-col border border-[#E6E6E6] font-poppins">
-        <div className="flex flex-row">
-          <h1 className="text-[#1A1A1A] font-semibold text-[20px]">Recent Order History</h1>
+        <div className="flex flex-col justify-between border border-[#E6E6E6] font-poppins w-[70%] p-4 mx-auto rounded-md">
+        <div className="flex flex-row justify-between">
+          <h1 className="text-[#1A1A1A] font-semibold text-[20px] py-5">Recent Order History</h1>
           <button className="text-[#20B526] text-sm">View All</button>
         </div>
         <table>
     <thead>
-        <tr className="bg-[#F2F2F2] text-[#4D4D4D] text-sm">
-           <th>ORDER ID</th>
-           <th>DATE</th>
-           <th>tOTAL</th>
-           <th>STATUS</th>
+        <tr className="bg-[#F2F2F2] text-[#4D4D4D] text-sm py-6">
+           <th className="text-left">ORDER ID</th>
+           <th className="text-left">DATE</th>
+           <th className="text-left">TOTAL</th>
+           <th className="text-left">STATUS</th>
            <th></th>
         </tr>
     </thead>
     <tbody>
     {orders.map((order) => (
     <tr key={order.id}>
-        <td>{order.id}</td>
-        <td>{order.date}</td>
-        <td>{order.total}</td>
-        <td>{order.status}</td>
-        <td><a href={`/orders/${order.id}`}>View Details</a></td>
+        <td className="py-3 text-[#333333] text-sm">{order.id}</td>
+        <td className="py-3 text-[#333333] text-sm">{order.date}</td>
+        <td className="py-3 text-[#333333] text-sm">{order.total}</td>
+        <td className="py-3 text-[#333333] text-sm">{order.status}</td>
+        <td className="text-[#20B526] text-sm"><a href={`/orders/${order.id}`}>View Details</a></td>
     </tr>
 ))}
     </tbody>
