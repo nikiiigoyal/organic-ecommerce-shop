5ffbbbec7d2c5197307a463b9ee05107e7985215
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-25 20:36:30 +0530

    Design of settings page

--- src/components/pages/settings/ChangePassword.jsx
+++ src/components/pages/settings/ChangePassword.jsx
@@ -2,6 +2,7 @@ export const ChangePassword = () => {
     return(
         <>
         <div>
+            <h1>Change Password</h1>
             <label>
                 <input placeholder="Change Password"></input>
             </label>
