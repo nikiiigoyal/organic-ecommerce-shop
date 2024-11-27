97aa60063f6fe75378dfa8d603e41cfdbda082cb
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-27 19:03:29 +0530

    New design in billing page

--- src/components/pages/settings/ChangePassword.jsx
+++ src/components/pages/settings/ChangePassword.jsx
@@ -1,23 +1,23 @@
 export const ChangePassword = () => {
     return(
         <>
-        <div className="border border-#E6E6E6]">
-            <h1 className="text-sm text-[#1A1A1A]">Change Password</h1>
+        <div className="border border-#E6E6E6] rounded-md w-[70%]">
+            <h1 className="text-lg my-5 p-2 font-semibold text-[#1A1A1A] border border-b-[#E5E5E5]">Change Password</h1>
             <label>
-                <h2 className="text-[14px] text-[#1A1A1A]">Current Password</h2>
-                <input className="p-2 border border-[#E6E6E6]" placeholder="Password"></input>
+                <h2 className="text-[14px] text-[#1A1A1A] p-2">Current Password</h2>
+                <input className="p-2 border border-[#E6E6E6] rounded-md" placeholder="Password"></input>
             </label>
-            <div>
+            <div className="flex flex-row justify-between">
             <label>
-                <h2 className="text-[14px] text-[#1A1A1A]">New Password</h2>
-                <input className="p-2 border border-[#E6E6E6]" placeholder="Password"></input>
+                <h2 className="text-[14px] text-[#1A1A1A] p-2">New Password</h2>
+                <input className="p-2 border border-[#E6E6E6] rounded-md" placeholder="Password"></input>
             </label>
             <label>
-                <h2 className="text-[14px] text-[#1A1A1A]">Confirm Password</h2>
-                <input className="p-2 border border-[#E6E6E6]" placeholder="Password"></input>
+                <h2 className="text-[14px] text-[#1A1A1A] p-2">Confirm Password</h2>
+                <input className="p-2 border border-[#E6E6E6] mb-2 rounded-md" placeholder="Password"></input>
             </label>
             </div>
-            <button className="text-[#00B207] border border-[#00B207] rounded-lg p-4 text-sm">Change Password</button>
+            <button className="bg-[#00B207] text-white py-2 px-3 rounded-full my-2">Change Password</button>
         </div>
         </>
     )
