d3a20c0f3e5e512f708adf652c695fff13ff381a
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-27 16:46:09 +0530

    New file added for authentication

---
+++ src/components/AuthProvider.jsx
@@ -0,0 +1,32 @@
+import {supabase} from "../supabase"
+import { createContext, useContext, useEffect ,useState} from 'react'
+
+
+export const AuthContext = createContext({})
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    // Check active sessions and sets the user
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null)
+      setLoading(false)
+    })
+
+    // Listen for changes on auth state (signed in, signed out, etc.)
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+      setUser(session?.user ?? null)
+      setLoading(false)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
