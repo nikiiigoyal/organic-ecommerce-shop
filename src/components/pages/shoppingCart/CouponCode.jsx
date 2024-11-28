af92ee3c1888b1e261107a81bff99dd650e28242
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-28 18:24:00 +0530

    New design of shopping cart

---
+++ src/components/pages/shoppingCart/CouponCode.jsx
@@ -0,0 +1,10 @@
+const CouponCode = () => {
+    return(
+        <>
+        <div>
+            <h3 className="text-[#1A1A1A] text-[20px]">Coupon Code</h3>
+        </div>
+        </>
+    )
+}
+export default CouponCode;
