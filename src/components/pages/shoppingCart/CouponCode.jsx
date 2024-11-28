53964c4e96d7adb39a7ab242547905554be40a13
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-28 18:39:41 +0530

    New design of shopping cart

--- src/components/pages/shoppingCart/CouponCode.jsx
+++ src/components/pages/shoppingCart/CouponCode.jsx
@@ -1,8 +1,15 @@
 const CouponCode = () => {
     return(
         <>
-        <div>
-            <h3 className="text-[#1A1A1A] text-[20px]">Coupon Code</h3>
+        <div className="flex items-center border border-[#E6E6E6] mx-3 p-2 rounded-lg ">
+            <h3 className="text-[#1A1A1A] text-[20px] mr-2">Coupon Code</h3>
+            <label className="flex border border-[#E6E6E6] rounded-full p-2">
+            <input placeholder="Enter code" className="text-sm text-[#999999] p-2 rounded-full border border-[#FFFFFF]  "></input>
+            <button className="bg-[#333333] text-white text-[20px] px-9 py-3 rounded-full ">
+                Apply Coupon
+            </button>
+            </label>
+           
         </div>
         </>
     )
