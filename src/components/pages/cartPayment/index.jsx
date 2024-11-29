16beace8f4872b9610f49f9356b465c504731ae8
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-29 16:34:01 +0530

    New design of shopping cart

--- src/components/pages/cartPayment/index.jsx
+++ src/components/pages/cartPayment/index.jsx
@@ -1,9 +1,18 @@
+import { BillingInfo } from "../checkout/BillingInfo"
 import Payment from "../Payment"
+import AdditionalInfo from "./AdditionalInfo"
 
 export const CartPayment = () => {
     return (
         <>
-         <div className="mt-2 ml-auto p-2 border border-b-[#eee] rounded-lg w-[300px] mx-w-[1200px]">
+        <div className="flex">
+        <div className="flex flex-col justify-around">
+            <BillingInfo />
+            <AdditionalInfo />
+           {/* <h1>Additional Info</h1> */}
+           </div>
+         <div className="mt-2 ml-auto p-2 mr-4 border border-b-[#eee] rounded-lg w-[300px] mx-w-[1200px]">
+            <h1 className="text-[#1A1A1A] p-1 my-2 font-semibold text-[20px]">Order Summary</h1>
        
         <div className="subtotal flex flex-row justify-between my-3 border-b-2 pb-1">
           <span className="text-[#4D4D4D] text-sm">Subtotal:</span>
@@ -20,9 +29,13 @@ export const CartPayment = () => {
         {/* <div>
           <button className="bg-[#00B207] text-white px-4 py-2 rounded-full w-full">Proceed to checkout</button>
         </div> */}
+       
  <Payment /> 
-      </div>
+ 
+     
       
+      </div>
+      </div>
         </>
     )
 }
