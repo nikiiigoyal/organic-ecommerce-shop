9ba9d50747a7550b2e64fb33b04ed96b2c81294d
nikiiigoyal <nikiiigoyal@gmail.com>
2024-11-29 16:34:19 +0530

    New design of shopping cart

---
+++ src/components/pages/cartPayment/AdditionalInfo.jsx
@@ -0,0 +1,14 @@
+const AdditionalInfo = () => {
+    return(
+        <>
+        <div>
+      <h1 className="text-[#1A1A1A] text-md font-semibold">Additional Info</h1>
+      <p className="text-[#1A1A1A] text-sm">Order Notes (optional)</p>
+      <textarea className="text-sm text-[#999999] border border-[#E6E6E6] p-4 " placeholder="Notes about your order, e.g. special notes for delivery">
+        
+      </textarea>
+        </div>
+        </>
+    )
+}
+export default AdditionalInfo
