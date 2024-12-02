f4170061f714422edfaaedad761d8d3b10cddfe6
nikiiigoyal <nikiiigoyal@gmail.com>
2024-12-02 19:57:15 +0530

    Backend integration getting products from supabase in homepage

--- src/components/pages/homepage/sections/index.jsx
+++ src/components/pages/homepage/sections/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import BannerAndFeatures from "./Banner";
 import PopularCategories from "./PopularCategories";
-import { products } from "../../../../constants";
+// import { products } from "../../../../constants";
 import SaleImages from "./SaleImages";
 import HotDeals from "./HotDeals";
 import LatestNews from "./LatestNews";
@@ -11,8 +11,38 @@ import Follow from "./Follow";
 import DiscountBanner from "./DiscountBanner";
 import ProductsView from "./ProductsView";
 import { SimplePopup } from "../../NewsLetterPopUp";
+import { useEffect, useState } from "react";
+import { supabase } from "@/supabase";
 
 export default function Homepage ({onQuickView, addToWishlist}) {
+    const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("grocery_products")
+          .select("*");
+
+        if (error) throw error;
+
+        setProducts(data || []);
+        setLoading(false);
+      } catch (err) {
+        console.log("Error fetching products", err);
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) return <div>Loading products...</div>;
+  if (error) return <div>Error loading products: {error}</div>;
+
     return (
         <div>
             
