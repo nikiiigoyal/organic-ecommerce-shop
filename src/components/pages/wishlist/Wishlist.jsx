254b826d04d973219576beb701b57546b39e6188
nikiiigoyal <nikiiigoyal@gmail.com>
2024-12-04 19:26:37 +0530

    changes in wishlist

--- src/components/pages/wishlist/Wishlist.jsx
+++ src/components/pages/wishlist/Wishlist.jsx
@@ -1,34 +1,34 @@
 /* eslint-disable react/prop-types */
 
-import WishlistItem from "./WishlistItem"
+import WishlistItem from './WishlistItem';
 
-
-
-export const Wishlist = ({wishlist , onRemoveFromWishlist}) => {
-    return (
-        <>
-        <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-4xl mx-auto font-poppins">
-            <h1 className="text-2xl font-semibold text-center mb-6">My Wishlist</h1>
-            <div className="grid grid-cols-1 gap-6">
-                
-            <table className="w-full bg-white shadow-md rounded-lg font-poppins">
-      <thead>
-        <tr className="text-[#808080] text-sm font">
-          <th className="p-4 text-left">PRODUCT</th>
-          <th className="p-4 text-left">PRICE</th>
-          <th className="p-4 text-right">STOCK STATUS</th>
-          <th className="p-4 text-right"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {wishlist.map((product) => (
-          <WishlistItem key={product.id} product={product} onRemoveFromWishlist={onRemoveFromWishlist} />
-        ))}
-      </tbody>
-    </table>
-  
-            </div>
+export const Wishlist = ({ wishlist, onRemoveFromWishlist }) => {
+  return (
+    <>
+      <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-4xl mx-auto font-poppins">
+        <h1 className="text-2xl font-semibold text-center mb-6">My Wishlist</h1>
+        <div className="grid grid-cols-1 gap-6">
+          <table className="w-full bg-white shadow-md rounded-lg font-poppins">
+            <thead>
+              <tr className="text-[#808080] text-sm font">
+                <th className="p-4 text-left">PRODUCT</th>
+                <th className="p-4 text-left">PRICE</th>
+                <th className="p-4 text-right">STOCK STATUS</th>
+                <th className="p-4 text-right"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {wishlist.map((product) => (
+                <WishlistItem
+                  key={product.id}
+                  product={product}
+                  onRemoveFromWishlist={onRemoveFromWishlist}
+                />
+              ))}
+            </tbody>
+          </table>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
