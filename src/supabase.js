76f586d03e9a28ee87bba75b956a5c280e09893b
nikiiigoyal <nikiiigoyal@gmail.com>
2024-12-04 19:25:05 +0530

    changes in Layput

--- src/supabase.js
+++ src/supabase.js
@@ -1,10 +1,8 @@
 // src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_PROJECTURL
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_PROJECTURL;
 
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANONKEY;
 
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANONKEY
-
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
