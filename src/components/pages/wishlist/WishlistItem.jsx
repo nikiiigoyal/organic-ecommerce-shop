e39b5c15210dddd30ebd158d30e86fdf9c6ea3f3
nikiiigoyal <nikiiigoyal@gmail.com>
2024-12-05 08:56:26 +0530

    changes in design of header

--- src/components/pages/wishlist/WishlistItem.jsx
+++ src/components/pages/wishlist/WishlistItem.jsx
@@ -1,40 +1,47 @@
 /* eslint-disable react/prop-types */
-const WishlistItem = ({product , onRemoveFromWishlist}) => {
-    return(
-<>
-<tr>
-          <td className="p-4">
-            <div className="flex items-center">
-              <img src={product.image} alt={product.name} className="rounded-lg w-12 h-12 mr-4" />
-              <h2 className="text-[#1A1A1A] text-sm">{product.name}</h2>
-            </div>
-          </td>
-          <td className="p-4 text-[#1A1A1A] font-semibold text-sm">
-            ${product.price.toFixed(2)}
-            {product.oldPrice && (
-              <span className="line-through text-gray-400 ml-1">${product.oldPrice.toFixed(2)}</span>
-            )}
-          </td>
-          <td className="p-4 text-right">
-            <span className={`bg-${product.stockStatus === 'In Stock' ? 'green' : 'red'}-500 text-white py-1 px-2 rounded-lg`}>
-              {product.stockStatus === 'Available' ? 'in stock': 'out of stock'}
+const WishlistItem = ({ product, onRemoveFromWishlist }) => {
+  return (
+    <>
+      <tr>
+        <td className="p-4">
+          <div className="flex items-center">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="rounded-lg w-12 h-12 mr-4"
+            />
+            <h2 className="text-[#1A1A1A] text-sm">{product.name}</h2>
+          </div>
+        </td>
+        <td className="p-4 text-[#1A1A1A] font-semibold text-sm">
+          ${product.price.toFixed(2)}
+          {product.oldPrice && (
+            <span className="line-through text-gray-400 ml-1">
+              ${product.oldPrice.toFixed(2)}
             </span>
-          </td>
-          <td className="p-4 text-right">
-            <button className="bg-[#00B207] hover:bg-green-600 text-white text-sm py-2 px-5 rounded-full mr-2">
-              Add to Cart
-            </button>
-            <button
-              className=" hover:bg-red-600 text-white py-1 bg-black px-2 rounded-full"
-              onClick={() => onRemoveFromWishlist(product)}
-            >
-              <img src="/images/closeicon.svg" alt="Remove" className="w-5 h-5" />
-            </button>
-          </td>
-        </tr>
-     
+          )}
+        </td>
+        <td className="p-4 text-right">
+          <span
+            className={`bg-${product.stockStatus === 'In Stock' ? 'green' : 'red'}-500 text-white py-1 px-2 rounded-lg`}
+          >
+            {product.stockStatus === 'Available' ? 'in stock' : 'out of stock'}
+          </span>
+        </td>
+        <td className="p-4 text-right">
+          <button className="bg-[#00B207] hover:bg-green-600 text-white text-sm py-2 px-5 rounded-full mr-2">
+            Add to Cart
+          </button>
+          <button
+            className=" hover:bg-red-600 text-white py-1 bg-black px-2 rounded-full"
+            onClick={() => onRemoveFromWishlist(product)}
+          >
+            <img src="/images/closeicon.svg" alt="Remove" className="w-5 h-5" />
+          </button>
+        </td>
+      </tr>
 
- {/* <div className="bg-white shadow-md rounded-lg pg-4 flex">
+      {/* <div className="bg-white shadow-md rounded-lg pg-4 flex">
   <div className="relative">
   <img src={product.image} alt={product.name} className="rounded-lg p-5 mb-4"></img>
   
@@ -51,9 +58,7 @@ const WishlistItem = ({product , onRemoveFromWishlist}) => {
  <img src="/images/close.png" alt="Remove" className="w-4 h-4" />
   </button>
  </div> */}
-</>
-
-
-    )
-}
-export default WishlistItem
+    </>
+  );
+};
+export default WishlistItem;
