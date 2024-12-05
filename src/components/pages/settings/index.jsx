1fcd1ba339cda4fcdfcd92ce5199e609c4288b8e
nikiiigoyal <nikiiigoyal@gmail.com>
2024-12-05 11:12:56 +0530

    changes in design of settings page

--- src/components/pages/settings/index.jsx
+++ src/components/pages/settings/index.jsx
@@ -1,20 +1,21 @@
-import { BillingInfo } from "../checkout/BillingInfo"
+import { BillingInfo } from '../checkout/BillingInfo';
 
-import { AccountSettings } from "./AccountSettings"
-import { ChangePassword } from "./ChangePassword"
+import { AccountSettings } from './AccountSettings';
+import { ChangePassword } from './ChangePassword';
 
 export const Settings = () => {
-    return(
-        <>
+  return (
+    <>
+      <div>
+        <AccountSettings />
         <div>
-           
-            <AccountSettings/>
-            <div>
-            <BillingInfo />
-            <button className="bg-[#00B207] text-white py-2 px-3 rounded-full my-2">Save Changes</button>
-            </div>
-            <ChangePassword />
+          <BillingInfo />
+          <button className="bg-[#00B207] text-white py-2 px-3 rounded-full my-2">
+            Save Changes
+          </button>
         </div>
-        </>
-    )
-}
+        <ChangePassword />
+      </div>
+    </>
+  );
+};
