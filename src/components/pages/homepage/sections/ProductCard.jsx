08b15eb46660ff226b8dfbc4b20974ef198d89e8
nikiiigoyal <nikiiigoyal@gmail.com>
2024-12-05 11:15:54 +0530

    fetch data from backend for populat categories

--- src/components/pages/homepage/sections/ProductCard.jsx
+++ src/components/pages/homepage/sections/ProductCard.jsx
@@ -1,81 +1,97 @@
 /* eslint-disable react/prop-types */
-import { supabase } from "@/supabase";
-import { useEffect,useState } from "react";
-const ProductCard = ({ product, onQuickView ,addToWishlist }) => {
-    const [data, setData] = useState([])
-   const [loading, setLoading] = useState(true)
- const [error, setError] = useState(null)
+import { supabase } from '@/supabase';
+import { useEffect, useState } from 'react';
+const ProductCard = ({ product, onQuickView, addToWishlist }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // Fetch data
-  useEffect (() => {
+  useEffect(() => {
     const fetchData = async () => {
-        try {
-          const { data, error } = await supabase
-            .from('grocery_products')
-            .select('*')
-            
-          if (error) throw error
-          setData(data)
-        } catch (err) {
-          setError(err.message)
-        } finally {
-          setLoading(false)
-        }
+      try {
+        const { data, error } = await supabase
+          .from('grocery_products')
+          .select('*');
+
+        if (error) throw error;
+        setData(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
       }
-      fetchData()
-     
-  },[])
-  console.log(data)
-    
-    const handleAddToWishlist = () => {
-        addToWishlist(product)
-        console.log("clickeddd")
-    }
-    return (
-        <div key={product.id} className="border p-4 rounded shadow-md relative flex flex-col hover:border-green-500 transition duration-300">
-            <div className="relative group">
-                <img src={product.image_urls} alt={product.name} className="mb-4 transition-transform duration-300 group-hover:scale-105" />
+    };
+    fetchData();
+  }, []);
+  console.log(data);
 
-                {/* Heart and eye icons */}
-                <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 flex flex-col justify-around pt-5">
-                    <button  onClick={handleAddToWishlist}>
-                    <img src="/images/Heart.png" alt="favorite" className="w-6 h-6"
-                   
-                     />
-                    </button>
+  const handleAddToWishlist = () => {
+    addToWishlist(product);
+    console.log('clickeddd');
+  };
+  return (
+    <div
+      key={product.id}
+      className="border p-4 rounded shadow-md relative flex flex-col justify-between hover:border-green-500 transition duration-300"
+    >
+      <div className="relative group">
+        <img
+          src={product.image_urls}
+          alt={product.name}
+          className="mb-4 transition-transform duration-300 group-hover:scale-105"
+        />
 
-                    <button 
-                        onClick={() => onQuickView(product)} 
-                        className="text-white py-2 rounded hover:scale-150 transition duration-200"
-                    >
-                        <img src="/images/eye.png" alt="view" className="w-6 h-6" />
-                    </button>
-                </div>
-            </div>
+        {/* Heart and eye icons */}
+        <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 flex flex-col justify-around pt-5">
+          <button onClick={handleAddToWishlist}>
+            <img src="/images/Heart.png" alt="favorite" className="w-6 h-6" />
+          </button>
 
-            <div className="flex justify-between items-center">
-                <div>
-                    <h2 className="text-[14px] text-gray-700 font-semibold mt-2">{product.name}</h2>
-                    <div className="flex items-center space-x-2">
-                        <p className="text-[#1A1A1A]">${product.new_price}</p>
-                        {product.old_price && (
-                            <p className="line-through text-gray-400">{product.old_price.toFixed(2)}</p>
-                        )}
-                    </div>
-                    {product.sale && (
-                        <span className="text-white text-center absolute top-2 left-2 bg-[#EA4B48] p-1">{product.sale}% OFF</span>
-                    )}
-                    <div className="flex items-center mt-2">
-                        <span className="text-yellow-500">{'★'.repeat(product.rating)}</span>
-                    </div>
-                </div>
-                
-                <div className="relative group-hover:bg-green-500 p-1 rounded-full transition-all duration-300">
-                    <img src="/images/addToCart.png" alt="add to cart" className="w-8 h-8 group-hover:opacity-90" />
-                </div>
-            </div>
+          <button
+            onClick={() => onQuickView(product)}
+            className="text-white py-2 rounded hover:scale-150 transition duration-200"
+          >
+            <img src="/images/eye.png" alt="view" className="w-6 h-6" />
+          </button>
         </div>
-    );
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div>
+          <h2 className="text-[14px] text-gray-700 font-semibold mt-2">
+            {product.name}
+          </h2>
+          <div className="flex items-center space-x-2">
+            <p className="text-[#1A1A1A]">${product.new_price}</p>
+            {product.old_price && (
+              <p className="line-through text-gray-400">
+                {product.old_price.toFixed(2)}
+              </p>
+            )}
+          </div>
+          {product.sale && (
+            <span className="text-white text-center absolute top-2 left-2 bg-[#EA4B48] p-1">
+              {product.sale}% OFF
+            </span>
+          )}
+          <div className="flex items-center mt-2">
+            <span className="text-yellow-500">
+              {'★'.repeat(product.rating)}
+            </span>
+          </div>
+        </div>
+
+        <div className="relative group-hover:bg-green-500 p-1 rounded-full transition-all duration-300">
+          <img
+            src="/images/addToCart.png"
+            alt="add to cart"
+            className="w-8 h-8 group-hover:opacity-90"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
