3d96033900c238e2be277243d3cac0893813b668
nikiiigoyal <nikiiigoyal@gmail.com>
2025-01-02 13:44:29 +0530

    changes in skeleton loading page

--- src/components/skeleton/ProductSkeleton.jsx
+++ src/components/skeleton/ProductSkeleton.jsx
@@ -4,32 +4,32 @@ const EcobazarSkeleton = () => {
       className="w-full
      max-w-5xl mx-auto"
     >
-      {/* Header */}
+      {/* Header
       <div className="flex items-center justify-between p-4 border-b">
         {/* Logo */}
-        <div className="h-8 w-32 bg-gray-200 animate-pulse rounded"></div>
+      {/* <div className="h-8 w-32 bg-gray-200 animate-pulse rounded"></div> */}
 
-        {/* Search Bar */}
-        <div className="flex-1 max-w-xl mx-8">
+      {/* Search Bar */}
+      {/* <div className="flex-1 max-w-xl mx-8">
           <div className="h-12 bg-gray-200 animate-pulse rounded-lg"></div>
-        </div>
+        </div> */}
 
-        {/* Cart and Wishlist */}
-        <div className="flex gap-4">
+      {/* Cart and Wishlist */}
+      {/* <div className="flex gap-4">
           <div className="h-8 w-8 bg-gray-200 animate-pulse rounded-full"></div>
           <div className="h-8 w-24 bg-gray-200 animate-pulse rounded"></div>
         </div>
-      </div>
+      </div> */}
 
       {/* Navigation */}
-      <div className="flex items-center gap-6 p-4 bg-gray-50">
+      {/* <div className="flex items-center gap-6 p-4 bg-gray-50">
         {[...Array(5)].map((_, i) => (
           <div
             key={i}
             className="h-4 w-20 bg-gray-200 animate-pulse rounded"
           ></div>
         ))}
-      </div>
+      </div> */}
 
       {/* Main Content */}
       <div className="p-4">
